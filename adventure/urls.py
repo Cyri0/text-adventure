b36944d8652a7a1460a0a388de812e95c70b01b7
Cyri0 <nemestamas94@gmail.com>
2024-12-08 22:02:54 +0100

    state and chooce endpoints and views created

--- adventure/urls.py
+++ adventure/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("start/", views.start_game)
+    path("start/", views.start_game),
+    path("state/<str:player_id>", views.get_state),
+    path("choose/", views.choose)
 ]
--- adventure/views.py
+++ adventure/views.py
@@ -4,16 +4,19 @@ from . import models
 from . import serializers
 from rest_framework import status
 
-# @api_view(['GET'])
-# def start_game(request):
-#     player = models.PlayerStatus()
-#     player.current_location = models.Location.objects.get(id=1)
-#     player.health = 100
-#     player.save()
-
-#     serialized = serializers.PlayerCreatorSerializer(player, many=False)
-#     return Response(serialized.data)
-
+def player_response_builder(id, location, health, items):
+    choices_data = []
+    for choice in location.choices.all():
+        choices_data.append({
+            "id": choice.id,
+            "text": choice.text,
+        })
+    return {
+        "player_id": id,
+        "current_location": serializers.LocationSerializer(location).data,
+        "health": health,
+        "items": items,
+    }
 
 @api_view(['GET'])
 def start_game(request):
@@ -28,20 +31,37 @@ def start_game(request):
         items=[]
     )
 
-    location_data = serializers.LocationSerializer(starting_location).data
+    return Response(player_response_builder(player_status.player_id, starting_location, player_status.health, player_status.items), status=status.HTTP_201_CREATED)
 
-    choices_data = []
-    for choice in starting_location.choices.all():
-        choices_data.append({
-            "id": choice.id,
-            "text": choice.text,
-        })
+@api_view(['GET'])
+def get_state(request, player_id):
+    try:
+        player_status = models.PlayerStatus.objects.get(player_id = player_id)
+        return Response(player_response_builder(player_status.player_id, player_status.current_location, player_status.health, player_status.items), status=status.HTTP_200_OK)
+    except:
+         return Response({"error": "Játékos nem található"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    response_data = {
-        "player_id": player_status.player_id,
-        "current_location": location_data,
-        "health": player_status.health,
-        "items": player_status.items,
-    }
+@api_view(['POST'])
+def choose(request):
+    player_id = request.data.get('player_id')
+    choice_id = request.data.get('choice_id')
 
-    return Response(response_data, status=status.HTTP_201_CREATED)
+    if not player_id or not choice_id:
+        return Response({"error": "player_id és choice_id megadása szükséges."}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        player_status = models.PlayerStatus.objects.get(player_id=player_id)
+    except models.PlayerStatus.DoesNotExist:
+        return Response({"error": "Játékos nem található."}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        choice = player_status.current_location.choices.get(id=choice_id)
+    except models.Choice.DoesNotExist:
+        return Response({"error": "Érvénytelen választás."}, status=status.HTTP_400_BAD_REQUEST)
+
+    new_location = choice.next_location
+
+    player_status.current_location = new_location
+    player_status.save()
+
+
+    return Response(player_response_builder(player_status.player_id, new_location, player_status.health, player_status.items), status=status.HTTP_200_OK)
